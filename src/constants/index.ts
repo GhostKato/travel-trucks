f50a8296a13941127b94ca11c7276d225848d087
GhostKato <osadchenko.maksym@gmail.com>
2025-07-09 08:45:07 +0300

    fixed sprite

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,21 +2,21 @@ export const equipmentData: Record<
   string,
   { icon: string; label: string; type: 'boolean' | 'string' }
 > = {
-  AC: { icon: 'icon-wind', label: 'AC', type: 'boolean' },
+  AC: { icon: 'icon-ac', label: 'AC', type: 'boolean' },
   TV: { icon: 'icon-tv', label: 'TV', type: 'boolean' },
-  bathroom: { icon: 'icon-shower', label: 'Bathroom', type: 'boolean' },
-  kitchen: { icon: 'icon-cup-hot', label: 'Kitchen', type: 'boolean' },
+  bathroom: { icon: 'icon-bathroom', label: 'Bathroom', type: 'boolean' },
+  kitchen: { icon: 'icon-kitchen', label: 'Kitchen', type: 'boolean' },
   microwave: { icon: 'icon-microwave', label: 'Microwave', type: 'boolean' },
   radio: { icon: 'icon-radio', label: 'Radio', type: 'boolean' },
-  refrigerator: { icon: 'icon-fridge', label: 'Refrigerator', type: 'boolean' },
+  refrigerator: { icon: 'icon-refrigerator', label: 'Refrigerator', type: 'boolean' },
   water: { icon: 'icon-water', label: 'Water', type: 'boolean' },
   gas: { icon: 'icon-gas', label: 'Gas', type: 'boolean' },
-  engine: { icon: 'icon-fuel-pump', label: 'Engine', type: 'string' },
-  transmission: { icon: 'icon-diagram', label: 'Transmission', type: 'string' },
+  engine: { icon: 'icon-engine', label: 'Engine', type: 'string' },
+  transmission: { icon: 'icon-transmission', label: 'Transmission', type: 'string' },
 };
 
 export const typeData = [
-    { key: "van", label: "Van", icon: 'icon-grid-1x2' },
-    { key: "fully-integrated", label: "Fully Integrated", icon: 'icon-grid-3x3' },
-    { key: "alcove", label: "Alcove", icon: 'icon-grid' },
+    { key: "van", label: "Van", icon: 'icon-van' },
+    { key: "fully-integrated", label: "Fully Integrated", icon: 'icon-fully-integrated' },
+    { key: "alcove", label: "Alcove", icon: 'icon-alcove' },
   ];

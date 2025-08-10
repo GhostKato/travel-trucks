e11a88c24af7f3a42580617abd456e0f29768ce1
GhostKato <osadchenko.maksym@gmail.com>
2025-08-10 21:35:56 +0300

    edit not found page

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import s from './NotFoundPage.module.css'
 
 const NotFoundPage = () => {
+  const [countdown, setCountdown] = useState(5);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCountdown(prev => prev - 1);
+    }, 1000)
+
+    return () => clearInterval(timer); 
+  }, [])
+
+  useEffect(() => {
+    if (countdown === 0) {
+      navigate('/');
+    }
+  }, [countdown, navigate]);
+
   return (
-    <div>NotFoundPage</div>
+    <div className={s.container}>
+      <p className={s.p}>The page does not exist!</p>      
+      <p className={s.timer}>Redirecting in {countdown}...</p>
+    </div>
   )
 }
 
